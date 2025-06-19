883e9d44170b2384e4b6f22d76ca28ac0b02fc13
Ephraim Teodoro <114121386+EPHRAIMTEODORO@users.noreply.github.com>
2025-06-18 19:30:07 -1000

    Update ensure_java.sh

--- ensure_java.sh
+++ ensure_java.sh
@@ -1,10 +1,24 @@
 #!/usr/bin/env bash
 set -e
 
-JAVA_RELEASE=jdk-17.0.8
-JAVA_URL="https://download.java.net/java/GA/jdk17/9/GPL/openjdk-17_linux-x64_bin.tar.gz"
-mkdir -p "$NETLIFY_CACHE_DIR/$JAVA_RELEASE"
-curl -L "$JAVA_URL" | tar -xz -C "$NETLIFY_CACHE_DIR"
-export JAVA_HOME="$NETLIFY_CACHE_DIR/$JAVA_RELEASE"
-export PATH="$JAVA_HOME/bin:$PATH"
-java -version
+CACHE_DIR="$NETLIFY_BUILD_BASE/cache"
+JAVA_URL="https://download.java.net/java/GA/jdk17.0.2/dfd4a8d0985749f896bed50d7138ee7f/8/GPL/openjdk-17.0.2_linux-x64_bin.tar.gz"
+JAVA_RELEASE="jdk-17.0.2"
+
+current=$(java -version 2>&1 | head -n1 | cut -d'"' -f2 | cut -d'.' -f1)
+required="11"
+
+if [ ! "$(printf '%s\n' "$required" "$current" | sort -V | head -n1)" = "$required" ]; then
+  echo "Java $required+ required — upgrading"
+
+  if [ ! -d "$CACHE_DIR/$JAVA_RELEASE" ]; then
+    wget -qO openjdk.tar.gz "$JAVA_URL"
+    mkdir -p "$CACHE_DIR"
+    tar xf openjdk.tar.gz -C "$CACHE_DIR"
+  fi
+
+  export PATH="$CACHE_DIR/$JAVA_RELEASE/bin:$PATH"
+  java -version
+else
+  echo "Java $current is enough"
+fi
